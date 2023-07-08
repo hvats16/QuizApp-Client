c859af2507bc1d0f26e0bdc81ad59962c8d5d9d7
Harshit Vats <hvats16@gmail.com>
2023-07-08 21:13:59 +0530

    Quiz Component Added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function Quiz() {
-  return (
-    <div>
-      Quiz
-    </div>
-  )
+	function onNext() {
+		console.log("On next Click");
+	}
+	function onPrev() {
+		console.log("On next Click");
+	}
+	return (
+		<div className="container">
+			<h1 className="title text-light">Quiz Application</h1>
+			<div className="grid">
+				<button className="btn prev" onClick={onPrev}>
+					Prev
+				</button>
+				<button className="btn next" onClick={onNext}>
+					Next
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default Quiz
+export default Quiz;
